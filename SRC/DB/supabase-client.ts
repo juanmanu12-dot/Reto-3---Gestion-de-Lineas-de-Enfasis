abd345ae027fc0aaddfe486acaa2fe69b056d744
Juan Manuel M <juanmuzou@gmail.com>
2025-10-28 19:45:07 -0500

    agregue carpetas

--- SRC/DB/supabase-client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabaseClient = createClient(
-    "https://wfjafsykzsavyztefpex.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmamFmc3lrenNhdnl6dGVmcGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2MTA2MjQsImV4cCI6MjA3NzE4NjYyNH0.pnJUI7SjbxiNAo4BaenJDIbg7oJTpNsikhTa4wZ-u3o"
-);
-
-
